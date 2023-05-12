6aaadef4d4153f3ca3252e56786c2c94510f3cdf
nature011235 <87652464+nature011235@users.noreply.github.com>
2023-05-13 00:15:57 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const button=document.getElementsByClassName('menubutton')
+button[4].addEventListener('click',function(){
+    const maincontent=document.getElementById('maincontent');
+    maincontent.style.justifyContent='center';
+    maincontent.innerHTML=`<p style="text-align: center;">請先登入<a href="signin.html" style="color: aqua; text-decoration: none;">會員</a></p>`;
+})
+console.log(button[0])
+
+window.onload = function () {
+  let slideIndex = 1;
+  showSlide(slideIndex);
+
+  let prev = document.getElementById("prev");
+  prev.addEventListener("click", divideSlides, false);
+
+  let next = document.getElementById("next");
+  next.addEventListener("click", plusSlides, false);
+
+  const selectdot = document.querySelectorAll(".dot");
+  for (let i = 0; i < selectdot.length; i++) {
+    selectdot[i].addEventListener("click", function (e) {
+      showSlide((slideIndex = i + 1));
+    });
+  }
+
+  function plusSlides() {
+    showSlide((slideIndex += 1));
+  }
+
+  function divideSlides() {
+    showSlide((slideIndex -= 1));
+  }
+
+  function showSlide(num) {
+    let slides = document.getElementsByClassName("slide__item");
+    let dots = document.getElementsByClassName("dot");
+    if (num > slides.length) {
+      slideIndex = 1;
+    }
+
+    if (num < 1) {
+      slideIndex = slides.length;
+    }
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace("active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+  }
+};
